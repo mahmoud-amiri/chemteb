526ef4b086657b5ab653dfbaf02440438750ac4b
Mahmoud Amiri <mahmoud.amiri@uni-jena.de>
2025-03-07 11:56:41 +0100

    add more models

--- chem_eval.py
+++ chem_eval.py
@@ -39,86 +39,78 @@ def json_parser(data):
 if __name__ == "__main__":
     now = time.time()
 
-    models = {"google-bert/bert-base-uncased": "86b5e0934494bd15c9632b12f734a8a67f723594",
-              "allenai/scibert_scivocab_uncased": "24f92d32b1bfb0bcaf9ab193ff3ad01e87732fc1",
-              "nomic-ai/nomic-bert-2048": "no_revision_available",
-              "intfloat/e5-small": "e272f3049e853b47cb5ca3952268c6662abda68f",
-              "intfloat/e5-base": "b533fe4636f4a2507c08ddab40644d20b0006d6a",
-              "intfloat/e5-large": "4dc6d853a804b9c8886ede6dda8a073b7dc08a81",
-              "intfloat/e5-small-v2": "dca8b1a9dae0d4575df2bf423a5edb485a431236",
-              "intfloat/e5-base-v2": "1c644c92ad3ba1efdad3f1451a637716616a20e8",
-              "intfloat/e5-large-v2": "b322e09026e4ea05f42beadf4d661fb4e101d311",
-              "intfloat/multilingual-e5-small": "e4ce9877abf3edfe10b0d82785e83bdcb973e22e",
-              "intfloat/multilingual-e5-base": "d13f1b27baf31030b7fd040960d60d909913633f",
-              "intfloat/multilingual-e5-large": "4dc6d853a804b9c8886ede6dda8a073b7dc08a81",
-              "nomic-ai/nomic-embed-text-v1": "0759316f275aa0cb93a5b830973843ca66babcf5",
-              "nomic-ai/nomic-embed-text-v1.5": "b0753ae76394dd36bcfb912a46018088bca48be0",
-              "recobo/chemical-bert-uncased": "498698d28fcf7ce5954852a0444c864bdf232b64",
-              "BAAI/bge-m3": "5617a9f61b028005a4858fdac845db406aefb181",
-              "BAAI/bge-small-en": "2275a7bdee235e9b4f01fa73aa60d3311983cfea",
-              "BAAI/bge-base-en": "b737bf5dcc6ee8bdc530531266b4804a5d77b5d8",
-              "BAAI/bge-large-en": "abe7d9d814b775ca171121fb03f394dc42974275",
-              "BAAI/bge-small-en-v1.5": "5c38ec7c405ec4b44b94cc5a9bb96e735b38267a",
-              "BAAI/bge-base-en-v1.5": "a5beb1e3e68b9ab74eb54cfd186867f64f240e1a",
-              "BAAI/bge-large-en-v1.5": "d4aa6901d3a41ba39fb536a557fa166f842b0e09",
-              "all-mpnet-base-v2": "84f2bcc00d77236f9e89c8a360a00fb1139bf47d",
-              "multi-qa-mpnet-base-dot-v1": "3af7c6da5b3e1bea796ef6c97fe237538cbe6e7f",
-              "all-MiniLM-L12-v2": "a05860a77cef7b37e0048a7864658139bc18a854",
-              "all-MiniLM-L6-v2": "8b3219a92973c328a8e22fadcfa821b5dc75636a",
-              "m3rg-iitd/matscibert": "ced9d8f5f208712c4a90f98a246fe32155b29995",
-              "text-embedding-ada-002": "1",
-              "text-embedding-3-small": "1",
-              "text-embedding-3-large": "1",
-              "amazon-titan-embed-text-v1": "1",
-              "amazon-titan-embed-text-v2": "1",
-              "cohere-embed-english-v3": "1",
-              "cohere-embed-multilingual-v3": "1"
-              }
+    models = {
+                "sentence-transformers/bert-base-nli-mean-tokens": "no_revision_available",
+                "allenai/scibert_scivocab_uncased": "no_revision_available",
+                "nomic-ai/nomic-bert-2048": "40b98394640e630d5276807046089b233113aa87",
+                "intfloat/e5-small": "e272f3049e853b47cb5ca3952268c6662abda68f",
+                "intfloat/e5-base": "b533fe4636f4a2507c08ddab40644d20b0006d6a",
+                "intfloat/e5-large": "4dc6d853a804b9c8886ede6dda8a073b7dc08a81",
+                "intfloat/e5-small-v2": "dca8b1a9dae0d4575df2bf423a5edb485a431236",
+                "intfloat/e5-base-v2": "1c644c92ad3ba1efdad3f1451a637716616a20e8",
+                "intfloat/e5-large-v2": "b322e09026e4ea05f42beadf4d661fb4e101d311",
+                "intfloat/multilingual-e5-small": "fd1525a9fd15316a2d503bf26ab031a61d056e98",
+                "intfloat/multilingual-e5-base": "d13f1b27baf31030b7fd040960d60d909913633f",
+                "intfloat/multilingual-e5-large": "ab10c1a7f42e74530fe7ae5be82e6d4f11a719eb",
+                "nomic-ai/nomic-embed-text-v1": "0759316f275aa0cb93a5b830973843ca66babcf5",
+                "nomic-ai/nomic-embed-text-v1.5": "b0753ae76394dd36bcfb912a46018088bca48be0",
+                "recobo/chemical-bert-uncased": "no_revision_available",
+                "BAAI/bge-m3": "no_revision_available",
+                "BAAI/bge-small-en": "no_revision_available",
+                "BAAI/bge-base-en": "no_revision_available",
+                "BAAI/bge-large-en": "no_revision_available",
+                "BAAI/bge-small-en-v1.5": "5c38ec7c405ec4b44b94cc5a9bb96e735b38267a",
+                "BAAI/bge-base-en-v1.5": "a5beb1e3e68b9ab74eb54cfd186867f64f240e1a",
+                "BAAI/bge-large-en-v1.5": "d4aa6901d3a41ba39fb536a557fa166f842b0e09",
+                "all-mpnet-base-v2": "no_revision_available",
+                "multi-qa-mpnet-base-dot-v1": "no_revision_available",
+                "all-MiniLM-L12-v2": "no_revision_available",
+                "all-MiniLM-L6-v2": "no_revision_available",
+                "m3rg-iitd/matscibert": "no_revision_available",
+                "allenai/specter": "no_revision_available",
+                "facebook/contriever": "no_revision_available",
+                "hkunlp/instructor-xl": "no_revision_available",
+                "facebook/contriever-msmarco": "no_revision_available",
+                "sentence-transformers/gtr-t5-base": "no_revision_available",
+                "sentence-transformers/gtr-t5-large": "no_revision_available",
+                "sentence-transformers/gtr-t5-xl": "no_revision_available",
+                "jinaai/jina-embeddings-v2-small-en": "no_revision_available",
+                "jinaai/jina-embeddings-v2-base-en": "no_revision_available",
+                "jinaai/jina-embeddings-v2-large-en": "no_revision_available",
+                "microsoft/mpnet-base": "no_revision_available",
+                "microsoft/MiniLM-L12-H384-uncased": "no_revision_available",
+                "sentence-transformers/all-MiniLM-L6-v2": "no_revision_available",
+                "sentence-transformers/all-MiniLM-L12-v2": "no_revision_available",
+                "sentence-transformers/paraphrase-multilingual-mpnet-base-v2": "79f2382ceacceacdf38563d7c5d16b9ff8d725d6",
+                "nomic-ai/nomic-embed-text-multilingual": "no_revision_available",
+                "BAAI/bge-multilingual": "no_revision_available",
+                "facebook/tart-full-flan-t5-xl": "no_revision_available",
+                "facebook/galactica-1.3b": "no_revision_available",
+                "microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract": "no_revision_available",
+                "microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract-fulltext": "no_revision_available",
+                "recobo/chemical-bert-uncased": "no_revision_available",
+                "m3rg-iitd/matscibert": "no_revision_available",
+                "VAMPIRE/PubChemBERT": "no_revision_available",
+                "DeepChem/ChemBERTa-77M-MTR": "no_revision_available",
+                "ncfrey/ChemGPT-4.7M": "no_revision_available",
+                "sebastian-gehrmann/bio-mol-bert": "no_revision_available",
+                "microsoft/BiomedNLP-BiomedBERT-base-uncased-abstract": "no_revision_available",
+                "mistralai/Mistral-7B-v0.1": "no_revision_available",
+                "describeai/gemini": "no_revision_available",
+                "Cohere/Cohere-embed-english-v3.0": "no_revision_available",
+                "Cohere/Cohere-embed-multilingual-v3.0": "no_revision_available"
+            }
 
     all_tasks = [
-        # Pair Classification
-        "CoconutSMILES2FormulaPC",
-        "WikipediaAIParagraphsParaphrasePC",
-        "PubChemAISentenceParaphrasePC",
-        "PubChemSMILESCanonDescPC",
-        "PubChemSMILESCanonTitlePC",
-        "PubChemSMILESIsoDescPC",
-        "PubChemSMILESIsoTitlePC",
-        "PubChemSynonymPC",
-        "PubChemWikiParagraphsPC",
-        # Classification
-        "WikipediaEasy2GeneExpressionVsMetallurgyClassification",
-        "WikipediaEasy2GreenhouseVsEnantiopureClassification",
-        "WikipediaEasy2SolidStateVsColloidalClassification",
-        "WikipediaEasy2SpecialClassification",
-        "WikipediaEasy5Classification",
-        "WikipediaEasy10Classification",
-        "WikipediaEZ2Classification",
-        "WikipediaEZ10Classification",
-        "WikipediaHard2BioluminescenceVsLuminescenceClassification",
-        "WikipediaHard2IsotopesVsFissionProductsNuclearFissionClassification",
-        "WikipediaHard2SaltsVsSemiconductorMaterialsClassification",
-        "WikipediaMedium2BioluminescenceVsNeurochemistryClassification",
-        "WikipediaMedium2ComputationalVsSpectroscopistsClassification",
-        "WikipediaMedium2CrystallographyVsChromatographyTitrationpHClassification",
-        "WikipediaMedium5Classification",
-        "SDSEyeProtectionClassification",
-        "SDSGlovesClassification",
-        # Retrieval
+        # Retrieval 
         "ChemNQRetrieval",
         "ChemHotpotQARetrieval",
         "CoconutRetrieval"
-        # Bitext Mining
-        "CoconutSMILES2Formula1BM",
-        "CoconutSMILES2Formula2BM",
-        "PubChemSMILESISoTitleBM",
-        "PubChemSMILESCanonTitleBM",
-        "PubChemSMILESISoDescBM",
-        "PubChemSMILESCanonDescBM",
-        # Clustering
-        "WikipediaEasy10Clustering",
-        "WikipediaMedium5Clustering"
+        # "MedicalQARetrieval" ,
+        # "SCIDOCS",
+        # "TRECCOVID"
     ]
+
 
     tasks = mteb.get_tasks(tasks=all_tasks)
 
@@ -136,6 +128,10 @@ if __name__ == "__main__":
             wandb.init(project='Chembedding - Benchmarking', name=model_name,
                     config={"revision": model_rev})
             model = mteb.get_model(model_full_name)
+            # 🔹 Fix for missing padding token
+            if model.tokenizer.pad_token is None:
+                model.tokenizer.pad_token = model.tokenizer.eos_token
+                print(f"Set PAD token for {model_full_name}")
             evaluation = mteb.MTEB(tasks=tasks)
             evaluation.run(model, output_folder="chem_results",
                         overwrite_results=False)
